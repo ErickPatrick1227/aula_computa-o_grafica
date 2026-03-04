f678d981a36e15387620c819211e881d4f0546c3
Erick Patrick <erickckpatrick17@gmail.com>
2026-03-04 20:50:37 -0300

    Add files via upload

---
+++ aula0403.py
@@ -0,0 +1,17 @@
+import numpy as np
+import time
+
+N = 1_000_000
+lista_a = [1.0]  N
+lista_b = [2.0]  N
+t0 = time.time()
+resultado = [a + b for a,b in zip(lista_a, lista_b)]
+t1 = time.time()
+print(fPython List {(t1-t0)1000.1f} ms)
+
+arr_a = np.ones(N, dtype=np.float64)
+arr_b = np.full(N, 2.0,  dtype=np.float64)
+t0 = time.time()
+resultado = arr_a + arr_b
+t1 = time.time()
+print(fNump array {(t1-t0)1000.1f} ms)
